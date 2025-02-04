8770b103be5cc668c1fc158536b4e435aa5cc8b9
Milki Legesse <nitfanono@gmail.com>
2025-02-04 11:49:29 +0300

    Created 'Valid Anagram' by A2SV Hub

---
+++ 04-Feb-2025/Valid Anagram 239455.py
@@ -0,0 +1,9 @@
+# Problem: Valid Anagram - https://leetcode.com/problems/valid-anagram/description/
+
+from collections import Counter
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return Counter(s) == Counter(t)
+            
+        
