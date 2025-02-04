d212579edb8139ee72bd4332188e1e9621d82f6c
Milki Legesse <nitfanono@gmail.com>
2025-02-04 14:06:34 +0300

    Created 'Love Story' by A2SV Hub

---
+++ 04-Feb-2025/Love Story 239609.py
@@ -0,0 +1,15 @@
+# Problem: Love Story - https://codeforces.com/contest/1829/problem/A
+
+compare = "codeforces"
+
+for _ in range(int(input())):
+    counter = 0
+    word = input()
+    for i in range(10):
+            if(compare[i] != word[i]):
+                counter+=1
+    print( counter )
+
+    
+
+    
