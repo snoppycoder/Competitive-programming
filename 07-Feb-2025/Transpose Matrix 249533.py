9e0158470c92fbaf96787d1e8ebe4b9855b068ca
Milki Legesse <nitfanono@gmail.com>
2025-02-07 14:23:41 +0300

    Created 'Transpose Matrix' by A2SV Hub

---
+++ 07-Feb-2025/Transpose Matrix 249533.py
@@ -0,0 +1,6 @@
+# Problem: Transpose Matrix - https://leetcode.com/problems/transpose-matrix/
+
+class Solution:
+    def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
+        return [list(rows) for rows in zip(*matrix)]
+        
