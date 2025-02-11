845fd06c44add51aa6d53ad8e69fc0cee11d1c88
Milki Legesse <nitfanono@gmail.com>
2025-02-11 14:16:55 +0300

    Created 'Bubble Sort' by A2SV Hub

---
+++ 11-Feb-2025/Bubble Sort 256293.py
@@ -0,0 +1,37 @@
+# Problem: Bubble Sort - https://www.hackerrank.com/challenges/ctci-bubble-sort/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countSwaps' function below.
+#
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def countSwaps(a):
+    counter = 0
+    for i in range(len(a)):
+        for j in range(len(a) - i-1):
+            if(a[j] > a[j+1]):
+                a[j], a[j+1] = a[j+1], a[j]
+                counter+=1
+                
+    print(f"Array is sorted in {counter} swaps.")
+    print(f"First Element: {a[0]}")
+    print(f"Last Element: {a[-1]}")
+    
+    # Write your code here
+    
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    countSwaps(a)
