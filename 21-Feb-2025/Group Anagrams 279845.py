6798751116e84a845a77e699bf6c20182e614eab
Milki Legesse <nitfanono@gmail.com>
2025-02-21 22:50:34 +0300

    Created 'Group Anagrams' by A2SV Hub

---
+++ 21-Feb-2025/Group Anagrams 279845.py
@@ -0,0 +1,13 @@
+# Problem: Group Anagrams - https://leetcode.com/problems/group-anagrams/
+
+from collections import defaultdict
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagrams = defaultdict(list)
+        
+        for word in strs:
+            key = tuple(sorted(word)) 
+            anagrams[key].append(word)  
+        
+        return list(anagrams.values())
