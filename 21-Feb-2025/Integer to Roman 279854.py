cbf191661012254f411ce5a8c84a256686137836
Milki Legesse <nitfanono@gmail.com>
2025-02-21 22:52:39 +0300

    Created 'Integer to Roman' by A2SV Hub

---
+++ 21-Feb-2025/Integer to Roman 279854.py
@@ -0,0 +1,19 @@
+# Problem: Integer to Roman - https://leetcode.com/problems/integer-to-roman/description/
+
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        
+        ref = [
+            (1000, "M"), (900, "CM"), (500, "D"), (400, "CD"),
+            (100, "C"), (90, "XC"), (50, "L"), (40, "XL"),
+            (10, "X"), (9, "IX"), (5, "V"), (4, "IV"), (1, "I")
+        ]
+        
+        result = []
+        
+        for value, symbol in ref:
+            while num >= value:
+                num -= value
+                result.append(symbol)
+        
+        return "".join(result)
