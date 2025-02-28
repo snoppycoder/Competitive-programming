c75b67a8cdb06f587ba7540422b44befaf6a5199
Milki Legesse <nitfanono@gmail.com>
2025-02-28 22:38:34 +0300

    Created 'Odd Even Linked List' by A2SV Hub

---
+++ 28-Feb-2025/Odd Even Linked List 287321.py
@@ -0,0 +1,22 @@
+# Problem: Odd Even Linked List - https://leetcode.com/problems/odd-even-linked-list/
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        odd = head
+        if head:
+            even = head.next
+        else:
+            return head
+        even_head = even
+        while even and even.next:
+            odd.next = even.next
+            odd = odd.next
+            even.next = odd.next
+            even = even.next
+        odd.next = even_head
+        return head
