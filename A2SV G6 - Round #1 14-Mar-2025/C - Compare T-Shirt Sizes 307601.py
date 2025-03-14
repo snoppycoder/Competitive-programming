2c06e3ef8b34f028433acd61fc2798c82a323e23
Milki Legesse <nitfanono@gmail.com>
2025-03-15 01:01:20 +0300

    Created 'C - Compare T-Shirt Sizes' by A2SV Hub

---
+++ A2SV G6 - Round #1 14-Mar-2025/C - Compare T-Shirt Sizes 307601.py
@@ -0,0 +1,26 @@
+# Problem: C - Compare T-Shirt Sizes - https://codeforces.com/gym/585107/problem/C
+
+def compare_sizes(a, b):
+    
+    if a == b:
+        return "="
+
+    if a == "M":
+        return ">" if b[-1] == "S" else "<"
+    if b == "M":
+        return "<" if a[-1] == "S" else ">"
+    
+  
+    if a[-1] == "S" and b[-1] == "S":
+        return "<" if len(a) > len(b) else ">"
+    
+   
+    if a[-1] == "L" and b[-1] == "L":
+        return ">" if len(a) > len(b) else "<"
+    
+   
+    return "<" if a[-1] == "S" else ">"
+t = int(input())  
+for _ in range(t):
+    a, b = input().split()
+    print(compare_sizes(a, b))
