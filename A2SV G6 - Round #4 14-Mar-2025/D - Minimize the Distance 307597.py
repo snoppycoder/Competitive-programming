c77b408e679254e2eb4fe100b3407a6a060faa5a
Milki Legesse <nitfanono@gmail.com>
2025-03-15 00:55:22 +0300

    Created 'D - Minimize the Distance' by A2SV Hub

---
+++ A2SV G6 - Round #4 14-Mar-2025/D - Minimize the Distance 307597.py
@@ -0,0 +1,18 @@
+# Problem: D - Minimize the Distance - https://codeforces.com/gym/590053/problem/D
+
+from sys import stdin
+import math
+def inp(): return stdin.readline().strip()
+def ls(): return [int(i) for i in inp().split()]
+def mt(rows): return[list(map(int, inp().split())) for _ in range(rows)]
+n = int(inp())
+nums = ls()
+nums.sort()
+
+index = n//2
+if n % 2 == 0:
+
+    print(nums[index-1])
+else:
+    print(nums[index])
+
