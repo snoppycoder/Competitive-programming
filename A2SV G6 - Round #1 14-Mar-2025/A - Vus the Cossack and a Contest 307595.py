6fd62a079493c5987a052b94f3087d9845e076d5
Milki Legesse <nitfanono@gmail.com>
2025-03-15 00:54:25 +0300

    Created 'A - Vus the Cossack and a Contest' by A2SV Hub

---
+++ A2SV G6 - Round #1 14-Mar-2025/A - Vus the Cossack and a Contest 307595.py
@@ -0,0 +1,8 @@
+# Problem: A - Vus the Cossack and a Contest - https://codeforces.com/gym/585107/problem/A
+
+n = input().split()
+min_value = int(min(int(n[1]), int(n[2])))
+if(min_value >= int(n[0])):
+    print("Yes")
+else:
+    print("No")
